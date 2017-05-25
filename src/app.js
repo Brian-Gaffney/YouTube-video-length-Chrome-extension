4c512a6b1712d38388e6c70a0b0ab1e770aad64c
Brian Gaffney <brian@deb.ug>
2017-05-26 00:14:32 +1000

    Started working with dynamic data

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import throttle from 'lodash/throttle'
+
 import getAllYouTubeLinksOnPage from './utils/getAllYouTubeLinksOnPage'
 import {
 	showVideoData,
@@ -18,7 +20,45 @@ class App {
 
 		const youtubeLinks = getAllYouTubeLinksOnPage()
 
-		let videoIDs = this.getUniqueVideoIDs(youtubeLinks)
+		const videoIDs = this.getUniqueVideoIDs(youtubeLinks)
+
+		this.initializeDomMutationObserver()
+
+		// Nest each link under it's videoID
+		this.videoLinks = videoIDs.reduce((memo, videoID) => {
+			memo[videoID] = youtubeLinks.filter(link => link.videoID === videoID)
+			return memo
+		}, {})
+
+		loadVideoData(videoIDs)
+			.then(videoData => {
+				this.videoData = videoData
+			})
+			.then(() => {
+				showVideoData(this.videoLinks, this.videoData, this.countryCode)
+			})
+	}
+
+	// Listen for DOM changes on the <body>
+	// Run a throttled version of onDomMutated when changes occur
+	initializeDomMutationObserver () {
+		const domObserver = new MutationObserver(throttle(this.onDomMutated, 1000, {
+			leading: true,
+		}))
+
+		const observerConfig = { subtree: true, childList: true }
+
+		const body = document.querySelector('body')
+
+		domObserver.observe(body, observerConfig)
+	}
+
+	onDomMutated = () => {
+		console.log('### onDomMutated()')
+
+		const youtubeLinks = getAllYouTubeLinksOnPage()
+
+		const videoIDs = this.getUniqueVideoIDs(youtubeLinks)
 
 		// Nest each link under it's videoID
 		this.videoLinks = videoIDs.reduce((memo, videoID) => {
